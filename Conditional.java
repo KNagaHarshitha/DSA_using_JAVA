4be1977dcec03a5db6f47e079e9d7d59d9622b1d
KNagaHarshitha <nagaharshithakotha966@gmail.com>
2024-08-04 19:17:56 +0530

    Conditional loops

---
+++ Conditional.java
@@ -0,0 +1,385 @@
+import java.util.Scanner;
+public class Conditional {
+    // Basic Java Programs
+    // 1)Area Of Circle Java Program  (COMPLETED)
+    // 2)Area Of Triangle  (COMPLETED)
+    // 3)Area Of Rectangle Program   (COMPLETED)
+    // 4)Area Of Isosceles Triangle  (COMPLETED)
+    // 5)Area Of Parallelogram   (COMPLETED)
+    // 6)Area Of Rhombus    (COMPLETED)
+    // 7)Area Of Equilateral Triangle
+    // 8)Perimeter Of Circle
+    // 9)Perimeter Of Equilateral Triangle
+    // 10)Perimeter Of Parallelogram
+    // 11)Perimeter Of Rectangle
+    // 12)Perimeter Of Square
+    // 13)Perimeter Of Rhombus
+    // 14)Volume Of Cone Java Program
+    // 15)Volume Of Prism
+    // 16)Volume Of Cylinder
+    // 17)Volume Of Sphere
+    // 18)Volume Of Pyramid
+    // 19)Curved Surface Area Of Cylinder
+    // 20)Total Surface Area Of Cube
+    // 21)Fibonacci Series In Java Programs (COMPLETED)
+    // 22)Subtract the Product and Sum of Digits of an Integer  (COMPLETED)
+    // 23)Input a number and print all the factors of that number (use loops).  (COMPLETED)
+    // 24)Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)  (COMPLETED)
+    // 25)Take integer inputs till the user enters 0 and print the largest number from all.   (COMPLETED)
+    // 26)Addition Of Two Numbers  (COMPLETED)
+
+//Intermediate Java Programs
+        // 27)Factorial Program In Java (COMPLETED)
+        // 28)Calculate Electricity Bill
+        // 29)Calculate Average Of N Numbers  (COMPLETED)
+        // 30)Calculate Discount Of Product (COMPLETED)
+        // 31)Calculate Distance Between Two Points  (COMPLETED)
+        // 32)Calculate Commission Percentage
+        // 33)Power In Java   (COMPLETED)
+        // 34)Calculate Depreciation of Value
+        // 35)Calculate Batting Average
+        // 36)Calculate CGPA Java Program
+        // 37)Compound Interest Java Program
+        // 38)Calculate Average Marks (COMPLETED)
+        // 39)Sum Of N Numbers (COMPLETED)
+        // 40)Armstrong Number In Java (COMPLETED)
+        // 41)Find Ncr & Npr
+        // 42)Reverse A String In Java
+        // 42)Find if a number is palindrome or not (COMPLETED)
+        // 43)Future Investment Value
+        // 44)HCF Of Two Numbers Program
+        // 45)LCM Of Two Numbers
+        // 46)Java Program Vowel Or Consonant
+        // 47)Perfect Number In Java
+        // 48)Check Leap Year Or Not (COMPLETED)
+        // 49)Sum Of A Digits Of Number (COMPLETED)
+        // 50)Kunal is allowed to go out with his friends only on the even days of a given month. Write a program to count the number of days he can go out in the month of August.
+        // 51)Write a program to print the sum of negative numbers, sum of positive even numbers and the sum of positive odd numbers from a list of numbers (N) entered by the user. The list terminates when the user enters a zero.
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Conditional cl = new Conditional();
+
+
+
+        // System.out.println("Enter the length of the first diagonal of rhombus : ");
+        //  double p = sc.nextInt();
+        //  System.out.println("Enter the length of the second diagonal of rhombus: ");
+        //  double q = sc.nextInt();
+        // double ans = cl.areaOfRhombus(p,q);
+        // System.out.println("Area of the Parallelogram : "+ans);
+        // double p = sc.nextInt();
+
+
+        // System.out.println("Enter the length of the Parallelogram : ");
+        //  double l = sc.nextInt();
+        //  System.out.println("Enter the width of the Parallelogram : ");
+        //  double w = sc.nextInt();
+        // double ans = cl.areaOfParallelogram(l,w);
+        // System.out.println("Area of the Parallelogram : "+ans);
+        // // double p = sc.nextInt();
+        // double d = sc.nextInt();
+        // double ans = cl.discount(p,d);
+        // System.out.println("the discount price of the item : "+ ans);
+        // int n = sc.nextInt();
+        // int p = sc.nextInt();
+        // double ans = cl.powerOfANumber(n, p);
+        // System.out.println(ans);
+        // int n = sc.nextInt();
+        // boolean ans = cl.isArmstrong(n);
+        // System.out.println(ans);
+        // int n = sc.nextInt();
+        // double ans = cl.averageMarks(n);
+        // System.out.println("The average of"+n+ " subject marks is "+ans);
+        // int num = sc.nextInt();
+        // String ans = cl.isPalindrome(num);
+        // System.out.println(num + " "+ans);
+        // int n = sc.nextInt();
+        // int ans = cl.sumOfNNumber(n);
+        // System.out.println("Total sum of numbers yu entered : "+ans);
+        // int year = sc.nextInt();
+        // String ans = cl.leapYear(year);
+        // System.out.println(year+"is a "+ans);
+        // int num = sc.nextInt();
+        // int ans = cl.sumOfDigitsOfNumber(num);
+        // System.out.println("The sum of digits of a number "+num +" is "+ans);
+    // int a = sc.nextInt();
+    // int b = sc.nextInt();
+    // int ans = cl.distanceBetweenwtwoPoints(a,b);
+    // System.out.println("The distance between two pints is : "+ans);
+        // cl.averageOfNNumbers();
+
+        // int n = sc.nextInt();
+        // cl.factorialOfANumber(n);
+
+
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int ans = cl.additionOftwoNumber(a, b);
+        // System.out.println(ans);
+        // cl.largestNumberOfAll();
+        // cl.sumOfallNumbers();
+        // int n =  sc.nextInt();
+        // cl.factorsOfANumber(n);
+        //int n =  sc.nextInt();
+        // int ans = cl.subtractProductAndSumOfDigits(n);
+        // System.out.println(ans);
+        //  cl.fibonacciSeries(n);
+        //  System.out.println("Enter the base of the triangle : ");
+        //  double b = sc.nextInt();
+
+        //  System.out.println("Enter the height of the triangle : ");
+        //  double h = sc.nextInt();
+        //  double ans = cl.areaOfIsoscelesTriangle(b,h);
+        //  System.out.println("Area of the Isoscele triangle is : " + ans);
+        //  System.out.println("Enter the length of the rectangle : ");
+        //  double l = sc.nextInt();
+        //  System.out.println("Enter the width of the rectangle : ");
+        //  double w = sc.nextInt();
+        // double ans = cl.areaOfRectangle(l,w);
+        // System.out.println("Area of the Rectangle : "+ans);
+        // System.out.println("Enter the base of the triangle : ");
+        // double b = sc.nextInt();
+        // System.out.println("Enter the height of the triangle : ");
+        // double h = sc.nextInt();
+        // double ans = cl.areaOfTriangle(b,h);
+        // System.out.println("Area of the triangle is : " + ans);
+        // System.out.println("Enter the radius of a circle : ");
+        double radius = sc.nextDouble();
+        double ans = cl.areaOfCircle(radius);
+        System.out.println("Area of circle is : " + ans);
+        sc.close();
+    }
+    // 1)Area Of Circle Java Program
+    public double areaOfCircle(double r) {
+            return (22/7)*Math.pow(r,2);
+    }
+    // 2)Area Of Triangle
+    public double areaOfTriangle(double b,double h){
+        return 0.5*b*h;
+    }
+     // 3)Area Of Rectangle Program
+     public double areaOfRectangle(double l,double w){
+          return l*w;
+     }
+      // 4)Area Of Isosceles Triangle
+      public double areaOfIsoscelesTriangle(double b,double h){
+        return 0.5*b*h;
+    }
+     // 5)Area Of Parallelogram
+     public double areaOfParallelogram(double l,double w){
+        return l*w;
+   }
+   // 6) Area Of Rhombus
+   public double areaOfRhombus(double p,double q){
+    return (p*q)/2;
+}
+
+    // 21)Fibonacci Series In Java Programs
+    public void fibonacciSeries(int n){ 
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        if(n==0) System.out.println("Enter number above 0");
+        if(n==1) System.out.println(0);
+        if(n==2) System.out.print(0+" "+1);
+        if(n>2){
+            System.out.print(0+" "+1);
+        while(count<n){
+            int temp = b;
+            b=b+a;
+            a=temp;
+            count++;
+            
+            System.out.print(" " + b + " ");
+        }
+    }
+    }
+
+// 22)Subtract the Product and Sum of Digits of an Integer
+public int subtractProductAndSumOfDigits(int n){
+    int rem = 0;
+    int sum = 0;
+    int product = 1;
+    while(n>0){
+        rem = n%10;
+        n = n/10;
+        sum = sum + rem;
+        product = product*rem;
+    }
+    return product-sum;
+}
+// 23)Input a number and print all the factors of that number (use loops).
+public void factorsOfANumber(int n){
+    for(int i =1;i<=n;i++){
+        if(n%i==0){
+            System.out.print(i+" ");
+        }
+    }
+}
+
+
+ // 24)Take integer inputs till the user enters 0 and print the sum of all numbers (HINT: while loop)
+ public void sumOfallNumbers(){
+    Scanner ss = new Scanner(System.in);
+    int sum = 0;
+    int n = ss.nextInt();
+    while(n!=0){
+        sum = sum + n;
+        n = ss.nextInt();
+    }
+    System.out.println("Sum of all the number that the user entered is "+ sum);
+    ss.close();
+ }
+ // 25)Take integer inputs till the user enters 0 and print the largest number from all.
+ public void largestNumberOfAll(){
+    Scanner ss = new Scanner(System.in);
+    int n = ss.nextInt();
+    int largest = Integer.MIN_VALUE;
+    while(n!=0){
+        if(n>largest){
+            largest = n;
+            n=ss.nextInt();
+
+
+    }
+ }
+ System.out.println("The largest number of all that the user entered is "+largest);
+ ss.close();
+}
+// 26)Addition Of Two Numbers
+public int additionOftwoNumber(int a,int b){
+    return a+b;
+}
+    
+
+
+
+//Intermediate Java Programs
+        // 27) Factorial Program In Java
+        public void factorialOfANumber(int n){
+            int fact = 1;
+            for(int i=1;i<=n;i++){
+                fact = fact * i;
+                
+            }
+            System.out.println(fact);
+        }
+// 29)Calculate Average Of N Numbers
+public void averageOfNNumbers(){
+    Scanner ss = new Scanner(System.in);
+    int n = 0;
+    int sum = 0;
+    int count = 0;
+    System.out.print("Enter 'exit' to stop taking numbers : ");
+    while(true){
+        n = ss.nextInt();
+        sum = sum +n;
+       
+        count++;
+        String s = ss.next();
+        if(s.equals("exit")){
+           break;
+       
+        
+        }
+
+    }
+    int avg = (sum)/count;
+    System.out.println("The average of the numbers is "+avg);
+    ss.close();
+}
+
+
+ // 30)Calculate Discount Of Product
+ public double discount(double p,double d){
+    return (d/100)*p;
+ }
+// 31)Calculate Distance Between Two Points
+public int distanceBetweenwtwoPoints(int a,int b){
+    if(a>=b){
+        return a-b;
+    }
+    else{
+        return b-a;
+    }
+}
+
+// 33)Power In Java 
+public int powerOfANumber(int n,int p){
+    return (int)Math.pow(n,p);
+}
+// 38)Calculate Average Marks
+public double averageMarks(int n){
+    Scanner ss = new Scanner(System.in);
+    int sum = 0;
+    for(int i =0;i<n;i++){
+        int marks = ss.nextInt();
+        sum = sum + marks;
+
+    }
+    return sum/n;
+}
+// 39)Sum Of N Numbers
+public int sumOfNNumber(int n){
+    Scanner ss = new Scanner(System.in);
+
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        int num = ss.nextInt();
+        sum = sum + num;
+    }
+    return sum;
+
+}
+// 40)Armstrong Number In Java 
+public boolean isArmstrong(int n){
+    int original = n;
+    double sum = 0;
+    while(n>0){
+        int rem = n%10;
+        n=n/10;
+        sum =  sum + Math.pow(rem,3);
+    }
+    if(sum==original){
+        return true;
+    }
+    return false;
+}
+
+ // 42)Find if a number is palindrome or not
+ public String isPalindrome(int num){
+    int temp = num;
+    int sum = 0;
+    while(num>0){
+        int rem = num%10;
+        sum = (sum*10)+rem;
+        num = num/10;
+    }
+    if(temp==sum){
+        return "is a Palindrome";
+    }
+    else return "not a palindrome";
+ }
+
+ // 48)Check Leap Year Or Not
+ public String leapYear(int year){
+    if ((year%4==0) && (year%100!=0 || year%400==0)){
+        return "Leap Year";
+
+    }
+    else return "not a Leap Year";
+ }
+ // 49)Sum Of A Digits Of Number
+public int sumOfDigitsOfNumber(int num){
+    int sum = 0;
+    while(num>0){
+        int rem = num%10;
+        sum = sum + rem;
+        num = num/10;
+    }
+    return sum;
+}
+
+}
